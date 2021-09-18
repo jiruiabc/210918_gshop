60b794632930d781d9c8f023999146d250b1f97e
rui <1>
2021-09-18 18:42:19 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+      <router-view/>
+      <FooterGuide v-show="$route.meta.showFooter"/><!-- $route表示的当前路由， -->
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  components:{
+    FooterGuide
+  }
+};
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    width 100%
+    height 100%
+    background-color #ddd
+</style>
+
